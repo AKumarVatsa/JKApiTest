ed0bab484af0bfd41490fd2c0db7c3214b44c21c
AKumarVatsa <adarshvatsa7@gmail.com>
2025-07-07 09:07:13 +0530

    BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,5 +1,4 @@
 package base;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import io.restassured.RestAssured;
@@ -10,13 +9,11 @@ public class BaseTest {
 
 	    protected static ExtentReports report;
 	    protected ExtentTest test;
-
 	    @BeforeSuite
 	    public void init() {
 	        RestAssured.baseURI = config.ConfigManager.get("base.url");
 	        report = ExtentReportManager.initReport();
 	    }
-
 	    @AfterSuite
 	    public void tearDown() {
 	        report.flush();
