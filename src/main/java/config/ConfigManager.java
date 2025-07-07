b45da66088b54d74fbcb9b8c4c3c277be5003644
AKumarVatsa <adarshvatsa7@gmail.com>
2025-07-07 09:06:42 +0530

    ConfigManager.java

--- src/main/java/config/ConfigManager.java
+++ src/main/java/config/ConfigManager.java
@@ -1,11 +1,8 @@
 package config;
-
 import java.io.FileInputStream;
 import java.util.Properties;
 public class ConfigManager {
-
 	    private static Properties props = new Properties();
-
 	    static {
 	        try {
 	            FileInputStream fis = new FileInputStream("config.properties");
@@ -14,7 +11,6 @@ public class ConfigManager {
 	            throw new RuntimeException("Failed to load config.properties", e);
 	        }
 	    }
-
 	    public static String get(String key) {
 	        return props.getProperty(key);
 	    }
