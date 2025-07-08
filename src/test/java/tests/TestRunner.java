9307db1a5df2d5f9a7a7b69135e1e6ec1ca23839
AKumarVatsa <adarshvatsa7@gmail.com>
2025-07-08 16:54:55 +0530

    TestRunner.java

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -4,8 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-    features = "src/test/java/features",
-    glue = {"steps"},
+    features = "src/test/java/tests/features",
+    glue = {"src/test/java/tests/BookSteps.java"},
     plugin = { "pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json" },
     monochrome = true
 )
